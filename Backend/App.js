170d18c03fb57ff79ca05abd2a2666599229f253
Mohamed Hanfal <mohamedhanfal10@gmail.com>
2025-05-11 19:36:02 +0530

    first commit

---
+++ Backend/App.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config({ path: path.join(__dirname, "config", "config.env") });
+
+const products = require("./routes/product");
+const orders = require("./routes/order");
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started! Listening on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
+});
