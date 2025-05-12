03a439929fbc588aaab98d0af1b577293dd0eb86
Mohamed Hanfal <mohamedhanfal10@gmail.com>
2025-05-12 08:45:05 +0530

    feat: implement product retrieval APIs and add initial product data

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,14 +1,30 @@
-exports.getProducts = (req, res, next) => {
+const productModel = require("../models/productModel");
+
+//Get Products API - /api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await productModel.find({});
+
   res.json({
     success: true,
-    message: "Get products working!",
+    products,
+    message: "Get all products working!",
   });
 };
 
-exports.getSingleProducts = (req, res, next) => {
+//Get Single Product API - /api/v1/products/:id
+exports.getSingleProducts = async (req, res, next) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+
     res.json({
       success: true,
+      product,
       message: "Get single product working!",
     });
-  };
-  
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
