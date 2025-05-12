16fd2df9dc38d0042c103110ed99a967e03db7f4
Mohamed Hanfal <mohamedhanfal10@gmail.com>
2025-05-12 09:37:24 +0530

    feat: add order model and create order functionality

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,6 +1,26 @@
-exports.createOrder = (req, res, next) => {
+const orderModel = require("../models/orderModel");
+
+//Create Order - /api/v1/order
+exports.createOrder = async (req, res, next) => {
+  const cartItems = req.body;
+  const amount = Number(
+    cartItems
+      .reduce((acc, item) => {
+        return acc + Number(item.product.price) * item.qty;
+      }, 0)
+      .toFixed(2)
+  );
+  const status = "Pending";
+
+  const order = await orderModel.create({
+    cartItems,
+    amount,
+    status,
+    createdAt: new Date(),
+  });
+
   res.json({
     success: true,
-    message: "Order working!",
+    order,
   });
 };
